6059ce3810a90ad8e8ba08b33273c5b369c10e7f
Everton Vieira <vieiraeverton33@hotmail.com>
2022-11-24 02:30:59 -0300

    feat: added global style

---
+++ src/styles/global.module.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+ 
+export default createGlobalStyle`
+:root {
+    --color-background: #0b0b0b;
+    --color-primary: #ff577f;
+    --color-primary-focus: #ff427f;
+    --color-primary-negative: #59323f;
+    --white: #ffffff;
+    --base-gray: #f8f9fa;
+    --primary-gray: #868e96;
+    --secondary-gray: #343b41;
+    --tertiary-gray: #212529;
+    --quaternary-gray: #121214; 
+}
+`
